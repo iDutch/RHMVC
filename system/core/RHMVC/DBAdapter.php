043641b8632f36011cfc4c3c8d169de16d733cf6
Richard Hoogstraaten <richard@digitaalkantoor.nl>
2016-10-17 17:34:32 +0200

    Fix spacing and whitespaces

--- system/core/RHMVC/DBAdapter.php
+++ system/core/RHMVC/DBAdapter.php
@@ -48,187 +48,209 @@ class DBAdapter
 
         $dsn = 'mysql:host=' . self::$dbsettings['hostname'] . ';dbname=' . self::$dbsettings['database'];
 
-		if (!isset(self::$instance[$dsn])) {
-			self::$instance[$dsn] = new self($dsn, self::$dbsettings['username'], self::$dbsettings['password']);
-		}
-		return self::$instance[$dsn];
-	}
+        if (!isset(self::$instance[$dsn])) {
+            self::$instance[$dsn] = new self($dsn, self::$dbsettings['username'], self::$dbsettings['password']);
+        }
+        return self::$instance[$dsn];
+    }
 
-	/**
-	 * Constructor
-	 */
-	private function __construct($dsn, $user, $pass) {
-		$this->connection = new \PDO($dsn, $user, $pass);
-	}
+    /**
+     * Constructor
+     */
+    private function __construct($dsn, $user, $pass)
+    {
+        $this->connection = new PDO($dsn, $user, $pass);
+    }
 
-	/**
-	 * Errorhandler
-	 * 
-	 * @param	object $query	PDO query object
-	 * 
-	 * TODO: Build error handler instead of dumping DB errors on screen
-	 */
-	private function handleError($query) {
-		if ($this->hasActiveTransaction) {
-			$this->rollback();
-		}
+    /**
+     * Errorhandler
+     *
+     * @param	object $query	PDO query object
+     *
+     * TODO: Build error handler instead of dumping DB errors on screen
+     */
+    private function handleError(PDOStatement $query, $sql, array $params = [], array $data = [])
+    {
+        if ($this->hasActiveTransaction) {
+            $this->rollback();
+        }
         if (self::SHOW_ERROR) {
-		    var_dump($query->errorInfo());
-		    echo '<pre>';
-		    var_dump($query->debugDumpParams());
-		    echo '</pre>';
+            echo "<textarea style='width: 100%; height: 200px;'>\n";
+            echo $query->errorInfo()[2] . "\n";
+            echo PDODebugger::getQuery($sql, $params, $data). "\n";
+            echo "</textarea>";
         }
-		exit;
-	}
+        exit;
+    }
 
-	/**
-	 * Delete statement builder
-	 * 
-	 * @param	string	$table	Database table
-	 * @param	array	$params	Custom field => array(value => value, type => type) as deletion criteria
-	 */
-	public function delete($table, array $params = array()) {
-		$sql = "DELETE FROM `" . $table . "` ";
+    /**
+     * Delete statement builder
+     * 
+     * @param	string	$table	Database table
+     * @param	array	$params	Custom field => array(value => value, type => type) as deletion criteria
+     */
+    public function delete($table, array $params = array())
+    {
+        $sql = "DELETE FROM `" . $table . "` ";
         $where = "WHERE ";
-		foreach ($params as $field => $array) {
-			if (is_array($array['value'])) {
+        foreach ($params as $field => $value) {
+            if (is_array($value)) {
                 $query_params = null;
-				foreach ($array['value'] as $k => $v) {
-                    $query_params .= ":p" . $k . ",";
-				}
-				$where .= "`" . $field . "` IN (" . substr($query_params, 0, -1) . ") AND ";
-			} else {
-				$where .= "`" . $field . "` = :" . $field . " AND ";
-			}
-		}
-		//Strip of last AND
-		$where = substr($where, 0, -5);
-		$query = $this->connection->prepare($sql . $where);
-
-		foreach ($params as $field => &$array) {
-			if (is_array($array['value'])) {
-				foreach ($array['value'] as $key => &$val) {
-					$query->bindParam(':p' . $key, $val);
-				}
-			} else {
-				$query->bindParam(':' . $field, $array['value'], isset($array['type']) ? $array['type'] : PDO::PARAM_STR);
-			}
-		}
-
-		if (!$query->execute()) {
-			$this->handleError($query);
-		}
-	}
-
-	/**
-	 * Select prepared statement
-	 * 
-	 * @param	string	$statement	SQL prepared statement 
-	 * @param	array	$params		field => value pairs as selection criteria
-     * @return  array   Result set
-	 */
-	public function query($statement, array $params = array()) {
-		$query = $this->connection->prepare($statement);
-		if (count($params)) {
-			foreach ($params as $field => &$array) {
-				$query->bindParam(':' . $field, $array['value'], isset($array['type']) ? $array['type'] : PDO::PARAM_STR);
-			}
-		}
-		if (!$query->execute()) {
-			$this->handleError($query);
-		}
-		return $query->fetchAll(PDO::FETCH_OBJ);
-	}
-
-	/**
-	 * Insert statement builder
-	 * 
-	 * @param	string	$table	Database table
-	 * @param	array	$params	Field => value pairs to insert
-     * @return  int     Last insert ID
-	 */
-	public function insert($table, array $params = array()) {
-		$sql = "INSERT INTO `" . $table . "` (`" . implode('`, `', array_keys($params)) . "`) VALUES (:" . implode(", :", array_keys($params)) . ")";
-		$query = $this->connection->prepare($sql);
-		if (count($params)) {
-			foreach ($params as $field => &$value) {
-				$query->bindParam(':' . $field, $value);
-			}
-		}
-		if (!$query->execute()) {
-			$this->handleError($query);
-		}
-		return $this->connection->lastInsertId();
-	}
-
-	/**
-	 * Update statement builder
-	 * 
-	 * @param	string	$table  Database table
-	 * @param	array	$data   field => value pairs to update
-	 * @param	array	$params	field => array(value => value, type => type) as update criteria
-	 */
-	public function update($table, array $data = array(), array $params = array()) {
-		$qry = null;
-        if (count($data)) {
-		    foreach ($data as $field => $value) {
-			    $qry.= ($qry != '' ? ',' : '') . "`" . $field . "` = :" . $field . "";
-		    }
+                foreach ($value as $k => $v) {
+                    $query_params .= ':p_' . $field . $k . ',';
+                }
+                $where .= '`' . $field . '` IN (' . substr($query_params, 0, -1) . ') AND ';
+            } else {
+                $where .= '`' . $field . '` = :' . $field . ' AND ';
+            }
         }
-		$sql = "UPDATE `" . $table . "` SET " . $qry . " ";
+        //Strip of last AND
+        $where = substr($where, 0, -5);
+        $query = $this->connection->prepare($sql . $where);
 
-		$where = "WHERE ";
-		foreach ($params as $field => $array) {
-			if (is_array($array['value'])) {
+        foreach ($params as $field => &$value) {
+            if (is_array($value)) {
+                foreach ($value as $key => &$val) {
+                    $query->bindParam(':p_' . $field . $key, $val);
+                }
+            } else {
+                $query->bindParam(':' . $field, $value, $this->getFieldType($value));
+            }
+        }
+
+        if (!$query->execute()) {
+            $this->handleError($query, $sql, $params);
+        }
+    }
+
+    /**
+     * Select prepared statement
+     *
+     * @param	string	$statement	SQL prepared statement 
+     * @param	array	$params		field => value pairs as selection criteria
+     * @return  array   Result set
+     */
+    public function query($statement, array $params = array())
+    {
+        $query = $this->connection->prepare($statement);
+        if (count($params)) {
+            foreach ($params as $field => &$value) {
+                $query->bindParam(':' . $field, $value, $this->getFieldType($value));
+            }
+        }
+        if (!$query->execute()) {
+            $this->handleError($query, $statement, $params);
+        }
+        return $query->fetchAll(PDO::FETCH_OBJ);
+    }
+
+    /**
+     * Insert statement builder
+     *
+     * @param	string	$table	Database table
+     * @param	array	$data	Field => value pairs to insert
+     * @return  int     Last insert ID
+     */
+    public function insert($table, array $data = array())
+    {
+        $sql = "INSERT INTO `" . $table . "` (`" . implode('`, `', array_keys($data)) . "`) VALUES (:" . implode(", :", array_keys($data)) . ")";
+        $query = $this->connection->prepare($sql);
+        if (count($data)) {
+            foreach ($data as $field => &$value) {
+                $query->bindParam(':' . $field, $value);
+            }
+        }
+        if (!$query->execute()) {
+            $this->handleError($query, $sql, [], $data);
+        }
+        return $this->connection->lastInsertId();
+    }
+
+    /**
+     * Update statement builder
+     *
+     * @param	string	$table  Database table
+     * @param	array	$data   field => value pairs to update
+     * @param	array	$params	field => array(value => value, type => type) as update criteria
+     */
+    public function update($table, array $data = array(), array $params = array())
+    {
+        $qry = null;
+        if (count($data)) {
+            foreach ($data as $field => $value) {
+                $qry.= ($qry != '' ? ',' : '') . "`" . $field . "` = :" . $field . "";
+            }
+        }
+        $sql = "UPDATE `" . $table . "` SET " . $qry . " ";
+
+        $where = "WHERE ";
+        foreach ($params as $field => $value) {
+            if (is_array($value)) {
                 $query_params = null;
-				foreach ($array['value'] as $k => $v) {
-                    $query_params .= ":p" . $k . ",";
-				}
-				$where .= "`" . $field . "` IN (" . substr($query_params, 0, -1) . ") AND ";
-			} else {
-				$where .= "`" . $field . "` = :" . $field . " AND ";
-			}
-		}
-		//Strip of last AND
-		$where = substr($where, 0, -5);
+                foreach ($value as $k => $v) {
+                    $query_params .= ':p_' . $field . $k . ',';
+                }
+                $where .= '`' . $field . '` IN (' . substr($query_params, 0, -1) . ') AND ';
+            } else {
+                $where .= '`' . $field . '` = :' . $field . ' AND ';
+            }
+        }
+        //Strip of last AND
+        $where = substr($where, 0, -5);
 
-		$query = $this->connection->prepare($sql . $where);
-		if (count($data)) {
-			foreach ($data as $field => &$value) {
-				$query->bindParam(':' . $field, $value);
-			}
-		}
-		foreach ($params as $field => &$array) {
-			if (is_array($array['value'])) {
-				foreach ($array['value'] as $key => &$val) {
-					$query->bindParam(':p' . $key, $val);
-				}
-			} else {
-				$query->bindParam(':' . $field, $array['value'], isset($array['type']) ? $array['type'] : PDO::PARAM_STR);
-			}
-		}
-		if (!$query->execute()) {
-			$this->handleError($query);
-		}
-	}
+        $query = $this->connection->prepare($sql . $where);
+        if (count($data)) {
+            foreach ($data as $field => &$value) {
+                $query->bindParam(':' . $field, $value);
+            }
+        }
+        foreach ($params as $field => &$value) {
+            if (is_array($value)) {
+                foreach ($value as $key => &$val) {
+                    $query->bindParam(':p_' . $field . $key, $val, $this->getFieldType($val));
+                }
+            } else {
+                $query->bindParam(':' . $field, $value,  $this->getFieldType($value));
+            }
+        }
+        if (!$query->execute()) {
+            $this->handleError($query, $sql . $where, $params, $data);
+        }
+    }
 
-	public function beginTransaction() {
-		if ($this->hasActiveTransaction) {
-			return false;
-		} else {
-			$this->hasActiveTransaction = $this->connection->beginTransaction();
-			return $this->hasActiveTransaction;
-		}
-	}
+    public function beginTransaction()
+    {
+        if ($this->hasActiveTransaction) {
+            return false;
+        } else {
+            $this->hasActiveTransaction = $this->connection->beginTransaction();
+            return $this->hasActiveTransaction;
+        }
+    }
 
-	public function commit() {
-		$this->connection->commit();
-		$this->hasActiveTransaction = false;
-	}
+    public function commit()
+    {
+        $this->connection->commit();
+        $this->hasActiveTransaction = false;
+    }
 
-	public function rollback() {
-		$this->connection->rollback();
-		$this->hasActiveTransaction = false;
-	}
+    public function rollback()
+    {
+        $this->connection->rollback();
+        $this->hasActiveTransaction = false;
+    }
+
+    private function getFieldType($value)
+    {
+        if (is_int($value)) {
+            return PDO::PARAM_INT;
+        } else if (is_null($value)) {
+            return PDO::PARAM_NULL;
+        } else if (is_bool($value)) {
+            return PDO::PARAM_BOOL;
+        } else {
+            return PDO::PARAM_STR;
+        }
+    }
 
 }
