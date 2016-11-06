161e16efaa63c88caae04a8967f920e44c899883
Richard Hoogstraaten <richard@hoostraaten.eu>
2016-11-06 10:57:03 +0100

    PSR-4 autoloading goodness.

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,9 +1,9 @@
 <?php
 session_start();
 
-//setlocale(LC_TIME, 'nl_NL');
+setlocale(LC_TIME, 'nl_NL');
 
-use core\RHMVC\Router;
+use System\Core\RHMVC\Router;
 
 if (isset($_SERVER['IS_DEVEL'])) {
     ini_set('display_errors', 1);
@@ -16,15 +16,6 @@ if (!file_exists(__DIR__ . '/../vendor/autoload.php')) {
     throw new Exception('RHMVC initialisation error: No composer autoload file found! Did you run: \'php composer.phar install\'?');
 }
 require __DIR__ . '/../vendor/autoload.php';
-
-require __DIR__ . '/../system/core/RHMVC/Router.php';
-require __DIR__ . '/../system/core/RHMVC/Route.php';
-require __DIR__ . '/../system/core/RHMVC/Translator.php';
-require __DIR__ . '/../system/core/RHMVC/AbstractController.php';
-require __DIR__ . '/../system/core/RHMVC/Helper.php';
-require __DIR__ . '/../system/core/RHMVC/View.php';
-
-require __DIR__ . '/../system/libs/Logger/Logger.php';
 
 ActiveRecord\Config::initialize(function($cfg)
 {
