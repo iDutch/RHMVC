43a9da91248110e3bab555c34e94882cf2fbcf92
Richard Hoogstraaten <richard@digitaalkantoor.nl>
2016-11-11 17:14:41 +0100

    Add articles through transactions

--- Application/Controllers/BlogController.php
+++ Application/Controllers/BlogController.php
@@ -52,12 +52,52 @@ class BlogController extends AbstractController
     {
         /* @var $Article \Application\Models\Article */
         $Article = $this->loadModel('Article');
+        /* @var $Article \Application\Models\ArticleContent */
+        $ArticleContent = $this->loadModel('ArticleContent');
         /* @var $Language \Application\Models\Language */
         $Language = $this->loadModel('Language');
+        /* @var $Category \Application\Models\Category */
+        $Category = $this->loadModel('Category');
+
+        if (isset($_POST['add_article'])) {
+            $article_data['allow_comments'] = false;
+            $article_data['is_online'] = false;
+            foreach ($_POST as $key => $value) {
+                if ($key == 'publish_date' || $key == 'archive_date' || $key == 'category_id' || $key == 'allow_comments' || $key == 'is_online') {
+                    $article_data[$key] = !empty($value) ? $value : null;
+                }
+                if ($key == 'language') {
+                    foreach ($value as $language_id => $lang_data) {
+                        $article_contents_data[$language_id]['is_online'] = false;
+                        foreach ($lang_data as $lang_key => $lang_value) {
+                            $article_contents_data[$language_id][$lang_key] = $lang_value;
+                        }
+                        $article_contents_data[$language_id]['language_id'] = $language_id;
+                    }
+                }
+            }
+            //var_dump($article_data, $article_contents_data); exit;
+            $saved = $Article->transaction(function() use ($article_data, $article_contents_data, $Article, $ArticleContent) {
+                $article = $Article::create($article_data);
+                if (!$article) {
+                    return false;
+                }
+                foreach ($article_contents_data as $article_contents_vars) {
+                    $article_contents_vars['article_id'] = $article->id;
+                    if (!$ArticleContent::create($article_contents_vars)) {
+                        return false;
+                    }
+                }
+            });
+            if ($saved) {
+                $this->redirect('/admin/blog/articles');
+            }
+        }
 
         $view = new View('blog/admin_articles_add.phtml');
         $view->setVars([
             'article' => $Article,
+            'categories' => $Category->find('all'),
             'languages' => $Language->find('all')
         ]);
 
