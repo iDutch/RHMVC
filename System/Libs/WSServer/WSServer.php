2ce7d0820e830f0c1e4a615dd3691e35ca25c954
Richard Hoogstraaten <richard@hoostraaten.eu>
2017-01-09 06:10:31 +0100

    Added Websocket server and notifications

---
+++ System/Libs/WSServer/WSServer.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace System\Libs\WSServer;
+
+use Ratchet\MessageComponentInterface;
+use Ratchet\ConnectionInterface;
+
+/**
+ * chat.php
+ * Send any incoming messages to all connected clients (except sender)
+ */
+class WSServer implements MessageComponentInterface
+{
+
+    protected $clients;
+
+    public function __construct()
+    {
+        $this->clients = new \SplObjectStorage;
+    }
+
+    public function onOpen(ConnectionInterface $conn)
+    {
+        $this->clients->attach($conn);
+    }
+
+    public function onMessage(ConnectionInterface $from, $msg)
+    {
+        foreach ($this->clients as $client) {
+            if ($from != $client) {
+                $client->send($msg);
+            }
+        }
+    }
+
+    public function onClose(ConnectionInterface $conn)
+    {
+        $this->clients->detach($conn);
+    }
+
+    public function onError(ConnectionInterface $conn, \Exception $e)
+    {
+        $conn->close();
+    }
+
+}
